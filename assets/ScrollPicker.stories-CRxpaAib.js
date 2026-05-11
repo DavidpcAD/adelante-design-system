1898a8ae063020c795afec1881fb54dcf273183b
danielavillalobosq <danielavillalobosq@users.noreply.github.com>
2026-05-11 15:22:23 +0000

    deploy: 579c34c967b00fb168d93ffd545cec1627a7d77d

---
+++ assets/ScrollPicker.stories-CRxpaAib.js
@@ -0,0 +1,25 @@
+import{n as e,o as t}from"./chunk-jRWAZmH_.js";import{N as n}from"./iframe-DEMm3koF.js";import{t as r}from"./jsx-runtime-DaLCRA3n.js";import{n as i,t as a}from"./ScrollPicker-B2_ggqsj.js";var o,s,c,l,u,d,f,p;e((()=>{o=t(n()),i(),s=r(),c={title:`Components/ScrollPicker`,component:a,parameters:{layout:`fullscreen`}},l=e=>{let[t,n]=(0,o.useState)(e.open),[r,i]=(0,o.useState)(e.initialValue);return(0,s.jsxs)(`div`,{style:{height:600,padding:24,background:`#f3f3f3`},children:[(0,s.jsxs)(`button`,{onClick:()=>n(!0),style:{padding:12},children:[`Abrir picker (valor actual: `,r,`)`]}),(0,s.jsx)(a,{...e,open:t,initialValue:r,onClose:()=>n(!1),onConfirm:e=>{i(e),n(!1)}})]})},u={args:{open:!1,initialValue:3,contextLabel:`CONECTOR ADAPTADOR HEMBRA EAGLE 110V`,variant:`default`},render:e=>(0,s.jsx)(l,{...e})},d={args:{open:!1,initialValue:10,contextLabel:`CEMENTO PORTLAND 50KG`,variant:`ok`},render:e=>(0,s.jsx)(l,{...e})},f={args:{open:!1,initialValue:25,contextLabel:`VARILLA DE HIERRO 12MM`,variant:`alert`},render:e=>(0,s.jsx)(l,{...e})},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    open: false,
+    initialValue: 3,
+    contextLabel: "CONECTOR ADAPTADOR HEMBRA EAGLE 110V",
+    variant: "default"
+  },
+  render: args => <Demo {...args} />
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    open: false,
+    initialValue: 10,
+    contextLabel: "CEMENTO PORTLAND 50KG",
+    variant: "ok"
+  },
+  render: args => <Demo {...args} />
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    open: false,
+    initialValue: 25,
+    contextLabel: "VARILLA DE HIERRO 12MM",
+    variant: "alert"
+  },
+  render: args => <Demo {...args} />
+}`,...f.parameters?.docs?.source}}},p=[`Default`,`Ok`,`Alert`]}))();export{f as Alert,u as Default,d as Ok,p as __namedExportsOrder,c as default};
---
+++ assets/TabsMenu.stories-BV7To8Z3.js
@@ -0,0 +1,51 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{N as t}from"./iframe-DEMm3koF.js";import{t as n}from"./jsx-runtime-DaLCRA3n.js";import{i as r,n as i,r as a,t as o}from"./TabsMenu-ehYpxWoc.js";var s,c,l,u,d,f,p,m,h;e((()=>{t(),r(),s=n(),c={title:`Core/TabsMenu`,component:a,tags:[`autodocs`],argTypes:{state:{control:`select`,options:[`standard`,`pressed`]},layout:{control:`select`,options:[`label`,`label+icon`]},icon:{control:`select`,options:[`home`,`check`,`stock`]}},args:{label:`Boleta`,state:`standard`,layout:`label+icon`,icon:`home`}},l={args:{layout:`label`}},u={args:{layout:`label+icon`}},d={args:{state:`pressed`}},f={render:()=>(0,s.jsxs)(`div`,{style:{display:`flex`,flexDirection:`column`,gap:12,padding:16},children:[(0,s.jsx)(a,{label:`Boleta`,state:`standard`,layout:`label+icon`,icon:`home`}),(0,s.jsx)(a,{label:`Boleta`,state:`pressed`,layout:`label+icon`,icon:`home`}),(0,s.jsx)(a,{label:`Opciones`,state:`standard`,layout:`label`}),(0,s.jsx)(a,{label:`Opciones`,state:`pressed`,layout:`label`})]})},p={name:`TabFilterChip / All states`,render:()=>(0,s.jsxs)(`div`,{style:{display:`flex`,gap:12,padding:16},children:[(0,s.jsx)(i,{label:`Aprobado`,state:`active`,icon:`check`}),(0,s.jsx)(i,{label:`Denegado`,state:`disabled`,icon:`close`}),(0,s.jsx)(i,{label:`Pendiente`,state:`disabled`,icon:`alert`})]})},m={name:`FilterBar`,render:()=>(0,s.jsx)(o,{chips:[{label:`Aprobado`,state:`active`,icon:`check`},{label:`Denegado`,state:`disabled`,icon:`close`},{label:`Pendiente`,state:`disabled`,icon:`alert`}]})},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    layout: "label"
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    layout: "label+icon"
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    state: "pressed"
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: "flex",
+    flexDirection: "column",
+    gap: 12,
+    padding: 16
+  }}>
+      <TabsMenu label="Boleta" state="standard" layout="label+icon" icon="home" />
+      <TabsMenu label="Boleta" state="pressed" layout="label+icon" icon="home" />
+      <TabsMenu label="Opciones" state="standard" layout="label" />
+      <TabsMenu label="Opciones" state="pressed" layout="label" />
+    </div>
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  name: "TabFilterChip / All states",
+  render: () => <div style={{
+    display: "flex",
+    gap: 12,
+    padding: 16
+  }}>
+      <TabFilterChip label="Aprobado" state="active" icon="check" />
+      <TabFilterChip label="Denegado" state="disabled" icon="close" />
+      <TabFilterChip label="Pendiente" state="disabled" icon="alert" />
+    </div>
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  name: "FilterBar",
+  render: () => <FilterBar chips={[{
+    label: "Aprobado",
+    state: "active",
+    icon: "check"
+  }, {
+    label: "Denegado",
+    state: "disabled",
+    icon: "close"
+  }, {
+    label: "Pendiente",
+    state: "disabled",
+    icon: "alert"
+  }]} />
+}`,...m.parameters?.docs?.source}}},h=[`Label`,`LabelWithIcon`,`Pressed`,`All`,`FilterChips`,`FilterBarExample`]}))();export{f as All,m as FilterBarExample,p as FilterChips,l as Label,u as LabelWithIcon,d as Pressed,h as __namedExportsOrder,c as default};

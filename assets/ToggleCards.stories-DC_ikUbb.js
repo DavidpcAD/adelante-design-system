975129d3cd6ebbf59cfee48760c325d9e7858bf5
danielavillalobosq <danielavillalobosq@users.noreply.github.com>
2026-05-11 16:23:28 +0000

    deploy: b488895c2d3bbfacd0be9381098de741437fb4ac

---
+++ assets/ToggleCards.stories-DC_ikUbb.js
@@ -0,0 +1,52 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{n as t,t as n}from"./ToggleCards-BK5Kw0k_.js";var r,i,a,o,s,c,l,u,d,f,p;e((()=>{t(),r={title:`Components/ToggleCards`,component:n,parameters:{layout:`centered`},argTypes:{size:{control:{type:`inline-radio`},options:[`big`,`small`]},visibility:{control:{type:`inline-radio`},options:[`open`,`close`]},state:{control:{type:`inline-radio`},options:[`standard`,`pressed`]},mode:{control:{type:`inline-radio`},options:[`normal`,`disabled`]}}},i={args:{size:`big`,visibility:`open`}},a={args:{size:`big`,visibility:`open`,state:`pressed`}},o={args:{size:`big`,visibility:`close`}},s={args:{size:`small`,visibility:`open`}},c={args:{size:`small`,visibility:`open`,state:`pressed`}},l={args:{size:`small`,visibility:`close`}},u={args:{size:`small`,visibility:`close`,state:`pressed`}},d={args:{size:`big`,visibility:`open`,mode:`disabled`}},f={args:{size:`big`,visibility:`open`,mode:`disabled`,state:`pressed`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "big",
+    visibility: "open"
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "big",
+    visibility: "open",
+    state: "pressed"
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "big",
+    visibility: "close"
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    visibility: "open"
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    visibility: "open",
+    state: "pressed"
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    visibility: "close"
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    visibility: "close",
+    state: "pressed"
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "big",
+    visibility: "open",
+    mode: "disabled"
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "big",
+    visibility: "open",
+    mode: "disabled",
+    state: "pressed"
+  }
+}`,...f.parameters?.docs?.source}}},p=[`BigOpen`,`BigOpenPressed`,`BigClose`,`SmallOpen`,`SmallOpenPressed`,`SmallClose`,`SmallClosePressed`,`Disabled`,`DisabledPressed`]}))();export{o as BigClose,i as BigOpen,a as BigOpenPressed,d as Disabled,f as DisabledPressed,l as SmallClose,u as SmallClosePressed,s as SmallOpen,c as SmallOpenPressed,p as __namedExportsOrder,r as default};

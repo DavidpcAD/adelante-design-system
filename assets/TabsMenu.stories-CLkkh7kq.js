ae8fd3997e05bba74d94bcc237f4c3a150dd23bc
DavidpcAD <DavidpcAD@users.noreply.github.com>
2026-05-13 22:44:53 +0000

    deploy: 7c815e9e07ba937a2e0a073c39e389179661b5fa

---
+++ assets/TabsMenu.stories-CLkkh7kq.js
@@ -0,0 +1,25 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{P as t}from"./iframe-DFWOD8j3.js";import{t as n}from"./jsx-runtime-DaLCRA3n.js";import{i as r,r as i}from"./TabsMenu-DYud6Q6A.js";var a,o,s,c,l,u,d;e((()=>{t(),r(),a=n(),o={title:`Sistema de Diseño/TabsMenu`,component:i,tags:[`autodocs`],argTypes:{state:{control:`select`,options:[`standard`,`pressed`]},layout:{control:`select`,options:[`label`,`label+icon`]},icon:{control:`select`,options:[`home`,`check`,`stock`]}},args:{label:`Boleta`,state:`standard`,layout:`label+icon`,icon:`home`}},s={args:{layout:`label`}},c={args:{layout:`label+icon`}},l={args:{state:`pressed`}},u={render:()=>(0,a.jsxs)(`div`,{style:{display:`flex`,flexDirection:`column`,gap:12,padding:16},children:[(0,a.jsx)(i,{label:`Boleta`,state:`standard`,layout:`label+icon`,icon:`home`}),(0,a.jsx)(i,{label:`Boleta`,state:`pressed`,layout:`label+icon`,icon:`home`}),(0,a.jsx)(i,{label:`Opciones`,state:`standard`,layout:`label`}),(0,a.jsx)(i,{label:`Opciones`,state:`pressed`,layout:`label`})]})},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    layout: "label"
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    layout: "label+icon"
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    state: "pressed"
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: "flex",
+    flexDirection: "column",
+    gap: 12,
+    padding: 16
+  }}>
+      <TabsMenu label="Boleta" state="standard" layout="label+icon" icon="home" />
+      <TabsMenu label="Boleta" state="pressed" layout="label+icon" icon="home" />
+      <TabsMenu label="Opciones" state="standard" layout="label" />
+      <TabsMenu label="Opciones" state="pressed" layout="label" />
+    </div>
+}`,...u.parameters?.docs?.source}}},d=[`Label`,`LabelWithIcon`,`Pressed`,`All`]}))();export{u as All,s as Label,c as LabelWithIcon,l as Pressed,d as __namedExportsOrder,o as default};

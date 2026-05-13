07238d40da05f7b3908ef3cb27a8154df09c3274
DavidpcAD <DavidpcAD@users.noreply.github.com>
2026-05-13 20:44:44 +0000

    deploy: 1f2a445c0899956c2a47f5de19c53a9047d562e5

---
+++ assets/mdx-react-shim-69iAXi32.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./react-Xki20yhD.js";var n=e((()=>{t()}));export{n as t};
--- assets/mdx-react-shim-Bkd9bZKd.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./react-Bjb5d9T6.js";var n=e((()=>{t()}));export{n as t};

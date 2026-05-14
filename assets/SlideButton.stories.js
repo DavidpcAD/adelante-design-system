a9cdeb489da2ebd035fd347d41188f259d7f400c
DavidpcAD <DavidpcAD@users.noreply.github.com>
2026-05-14 20:56:09 +0000

    deploy: 273c1f2ceeeee8420f435c0a0ce29ac627bb6a20

--- assets/SlideButton.stories.js
+++ assets/SlideButton.stories.js
@@ -1,14 +1,7 @@
-import{n as e}from"./chunk.js";import{n as t,t as n}from"./SlideButton.js";var r,i,a,o;e((()=>{t(),r={title:`Sistema de Diseño/SlideButton`,component:n,parameters:{layout:`centered`}},i={args:{label:`Pedir`,confirmedLabel:`Confirmado`,onConfirm:()=>console.log(`confirmed`)}},a={args:{label:`Pedir`,disabled:!0,disabledLabel:`Agregá un material`,onConfirm:()=>console.log(`not callable`)}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+import{n as e}from"./chunk.js";import{n as t,t as n}from"./SlideButton.js";var r,i,a;e((()=>{t(),r={title:`Sistema de Diseño/SlideButton`,component:n,parameters:{layout:`centered`}},i={args:{label:`Pedir`,confirmedLabel:`Confirmado`,onConfirm:()=>console.log(`confirmed`)}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
   args: {
     label: "Pedir",
     confirmedLabel: "Confirmado",
     onConfirm: () => console.log("confirmed")
   }
-}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: "Pedir",
-    disabled: true,
-    disabledLabel: "Agregá un material",
-    onConfirm: () => console.log("not callable")
-  }
-}`,...a.parameters?.docs?.source}}},o=[`Default`,`Disabled`]}))();export{i as Default,a as Disabled,o as __namedExportsOrder,r as default};
+}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
--- assets/ToggleCards.stories.js
+++ assets/ToggleCards.stories.js
@@ -1,4 +1,4 @@
-import{n as e}from"./chunk.js";import{n as t,t as n}from"./ToggleCards.js";var r,i,a,o,s,c,l,u,d;e((()=>{t(),r={title:`Sistema de Diseño/ToggleCards`,component:n,parameters:{layout:`centered`},argTypes:{size:{control:{type:`inline-radio`},options:[`big`,`small`]},visibility:{control:{type:`inline-radio`},options:[`open`,`close`]},state:{control:{type:`inline-radio`},options:[`standard`,`pressed`]},mode:{control:{type:`inline-radio`},options:[`normal`,`disabled`]}}},i={args:{size:`big`,visibility:`open`}},a={args:{size:`big`,visibility:`open`,state:`pressed`}},o={args:{size:`big`,visibility:`close`}},s={args:{size:`small`,visibility:`open`}},c={args:{size:`small`,visibility:`open`,state:`pressed`}},l={args:{size:`small`,visibility:`close`}},u={args:{size:`small`,visibility:`close`,state:`pressed`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+import{n as e}from"./chunk.js";import{n as t,t as n}from"./ToggleCards.js";var r,i,a,o,s,c,l,u,d,f,p,m,h,g,_;e((()=>{t(),r={title:`Sistema de Diseño/ToggleCards`,component:n,parameters:{layout:`centered`},argTypes:{size:{control:{type:`inline-radio`},options:[`big`,`small`]},visibility:{control:{type:`inline-radio`},options:[`open`,`close`]},state:{control:{type:`inline-radio`},options:[`standard`,`pressed`]},mode:{control:{type:`inline-radio`},options:[`normal`,`disabled`]}}},i={args:{size:`big`,visibility:`open`}},a={args:{size:`big`,visibility:`open`,state:`pressed`}},o={args:{size:`big`,visibility:`close`}},s={args:{size:`big`,visibility:`open`,mode:`disabled`}},c={args:{size:`big`,visibility:`open`,state:`pressed`,mode:`disabled`}},l={args:{size:`small`,visibility:`open`}},u={args:{size:`small`,visibility:`open`,state:`pressed`}},d={args:{size:`small`,visibility:`close`}},f={args:{size:`small`,visibility:`close`,state:`pressed`}},p={args:{size:`small`,visibility:`open`,mode:`disabled`}},m={args:{size:`small`,visibility:`open`,state:`pressed`,mode:`disabled`}},h={args:{size:`small`,visibility:`close`,mode:`disabled`}},g={args:{size:`small`,visibility:`close`,state:`pressed`,mode:`disabled`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
   args: {
     size: "big",
     visibility: "open"
@@ -16,24 +16,63 @@ import{n as e}from"./chunk.js";import{n as t,t as n}from"./ToggleCards.js";var r
   }
 }`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
   args: {
+    size: "big",
+    visibility: "open",
+    mode: "disabled"
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "big",
+    visibility: "open",
+    state: "pressed",
+    mode: "disabled"
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
     size: "small",
     visibility: "open"
   }
-}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
   args: {
     size: "small",
     visibility: "open",
     state: "pressed"
   }
-}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
   args: {
     size: "small",
     visibility: "close"
   }
-}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
   args: {
     size: "small",
     visibility: "close",
     state: "pressed"
   }
-}`,...u.parameters?.docs?.source}}},d=[`BigOpen`,`BigOpenPressed`,`BigClose`,`SmallOpen`,`SmallOpenPressed`,`SmallClose`,`SmallClosePressed`]}))();export{o as BigClose,i as BigOpen,a as BigOpenPressed,l as SmallClose,u as SmallClosePressed,s as SmallOpen,c as SmallOpenPressed,d as __namedExportsOrder,r as default};
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    visibility: "open",
+    mode: "disabled"
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    visibility: "open",
+    state: "pressed",
+    mode: "disabled"
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    visibility: "close",
+    mode: "disabled"
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    visibility: "close",
+    state: "pressed",
+    mode: "disabled"
+  }
+}`,...g.parameters?.docs?.source}}},_=[`BigOpen`,`BigOpenPressed`,`BigClose`,`BigDisabled`,`BigDisabledPressed`,`SmallOpen`,`SmallOpenPressed`,`SmallClose`,`SmallClosePressed`,`SmallDisabled`,`SmallDisabledPressed`,`SmallDisabledClose`,`SmallDisabledClosePressed`]}))();export{o as BigClose,s as BigDisabled,c as BigDisabledPressed,i as BigOpen,a as BigOpenPressed,d as SmallClose,f as SmallClosePressed,p as SmallDisabled,h as SmallDisabledClose,g as SmallDisabledClosePressed,m as SmallDisabledPressed,l as SmallOpen,u as SmallOpenPressed,_ as __namedExportsOrder,r as default};

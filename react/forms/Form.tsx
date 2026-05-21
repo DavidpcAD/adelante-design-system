59e629846d431fa27cbaa71b661374b327409ee4
storybook primera parte <davidpc@adelante.cr>
2026-05-21 16:45:12 -0600

    revert(checkbox): back to thick ring 8px + white bg + black icon

--- react/forms/Form.tsx
+++ react/forms/Form.tsx
@@ -128,7 +128,7 @@ export function CheckBox({
         transition={springs.snappy}
       >
         {isChecked && !isDisabled && (
-          <Icon name={iconName} size="lg" color="var(--ds-color-white)" />
+          <Icon name={iconName} size="lg" color="var(--ds-color-black)" />
         )}
       </motion.span>
       <span className="ds-checkbox__label">{label}</span>
